23fe2d12c6340cf72f7acadf244ec6d0a31b392c
nttwtkw <50012404+nttwtkw@users.noreply.github.com>
2022-03-22 12:14:42 +0700

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-task1= BashOperator(
-        task_id='Test_kubernetes_executor',
-        bash_command='echo Kubernetes',
-        queue = 'kubernetes'
-    )
-task2 = BashOperator(
-        task_id='Test_Celery_Executor',
-        bash_command='echo Celery',
-    )
