80978a96f285278e5451c381ce1b26e5f935fb99
sleepo-chin <56860530+sleepo-chin@users.noreply.github.com>
2022-03-24 14:06:58 +0700

    Update test_multi.py

--- test_multi.py
+++ test_multi.py
@@ -39,23 +39,23 @@ with DAG(
     is_paused_upon_creation=DAG_PAUSED_UPON_CREATION,
     default_args=default_args) as dag:
 
-@task(task_id="print_the_context")
-def print_context(ds=None, **kwargs):
-    """Print the Airflow context and ds variable from the context."""
-    pprint(kwargs)
-    print(ds)
-    return 'Whatever you return gets printed in the logs'
+    @task(task_id="print_the_context")
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        pprint(kwargs)
+        print(ds)
+        return 'Whatever you return gets printed in the logs'
 
-run_this = print_context()
+    run_this = print_context()
 
-# Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
-for i in range(5):
+    # Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
+    for i in range(5):
 
-    @task(task_id=f'sleep_for_{i}')
-    def my_sleeping_function(random_base):
-        """This is a function that will run within the DAG execution"""
-        time.sleep(random_base)
+        @task(task_id=f'sleep_for_{i}')
+        def my_sleeping_function(random_base):
+            """This is a function that will run within the DAG execution"""
+            time.sleep(random_base)
 
-    sleeping_task = my_sleeping_function(random_base=float(i) / 10)
+        sleeping_task = my_sleeping_function(random_base=float(i) / 10)
 
-    run_this >> sleeping_task
+        run_this >> sleeping_task
