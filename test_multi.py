a642a9f1babacaac0b2262c4dcc9192406cceaeb
sleepo-chin <56860530+sleepo-chin@users.noreply.github.com>
2022-03-24 14:33:28 +0700

    Update test_multi.py

--- test_multi.py
+++ test_multi.py
@@ -42,7 +42,7 @@ with DAG(
     @task(task_id="print_the_context")
     def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
-        pprint(kwargs)
+        print(kwargs)
         print(ds)
         return 'Whatever you return gets printed in the logs'
 
